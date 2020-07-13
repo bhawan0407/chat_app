a8205639b61a3b433323c23441d6554e4adde9bd
bhawan0407 <bhawandtu93@gmail.com>
2020-07-13 16:03:55 +0530

    Added login and logout functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,9 @@ require("semantic-ui-sass")
 
 $(document).on('turbolinks:load', function() {
   $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
 })
 
 
